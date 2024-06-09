537bc06c2b3ff480af6d6b504e0e0ad5a488cbbe
Milena Didenko <mneradovich05@gmail.com>
2024-06-09 19:04:20 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -19,15 +19,15 @@ function createBoxes() {
     alert('Please enter a number between 1 and 100.');
     return;
   }
+
+  let htmlString = '';
   let size = 30;
   for (let i = 0; i < amount; i++) {
-    const boxEl = document.createElement('div');
-    boxEl.style.width = `${size}px`;
-    boxEl.style.height = `${size}px`;
-    boxEl.style.backgroundColor = getRandomHexColor();
-    boxesEl.appendChild(boxEl);
+    const backgroundColor = getRandomHexColor();
+    htmlString += `<div style="width:${size}px; height:${size}px; background-color:${backgroundColor};"></div>`;
     size += 10;
   }
+  boxesEl.innerHTML = htmlString;
   inputEl.value = '';
 }
 
